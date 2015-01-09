e28c6fc091175139061cd8781a7699fdff536e0a
Kaz Smith <mr.hexor@gmail.com>
2015-01-08 20:05:26 -0600

    Added template for swerve drive class

---
+++ src/SwerveDrive.cpp
@@ -0,0 +1,34 @@
+/*
+ * SwerveDrive.cpp
+ *
+ *  Created on: Jan 8, 2015
+ *      Author: Kazimier
+ */
+
+#include "WPILib.h"
+
+class SwerveDrive
+{
+public:
+	SwerveDrive()
+	{
+
+	}
+
+	~SwerveDrive()
+	{
+
+	}
+
+	/*
+	 * angle: the direction relative to the field in which the
+	 * robot should drive; field forward is 0/360 degrees
+	 *
+	 * speed: double ranging from -1.0 to 1.0 which represents the speed at
+	 * which the wheels should be driven; -1.0 is full reverse, 1.0 is full forward
+	 */
+	void Drive(int angle, double speed)
+	{
+
+	}
+};
