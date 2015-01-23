0fda49fbd26773663473b342b042fa805b077547
Kaz Smith <mr.hexor@gmail.com>
2015-01-23 10:49:41 -0600

    Split SwerveDrive class into header and implementation

--- src/Team167Lib/SwerveDrive.cpp
+++ src/Team167Lib/SwerveDrive.cpp
@@ -5,197 +5,181 @@
  *      Author: Kazimier
  */
 
-#include "WPILib.h"
+#include "SwerveDrive.h"
 
-class SwerveDrive
+SwerveDrive::SwerveDrive(unsigned rotateEncLines, unsigned driveEncLines,
+                        unsigned feetToEncLinesR,
+                        unsigned short rotateTalon1Number,
+                        unsigned short rotateTalon2Number,
+                        unsigned short gyroChannel,
+                        int talon1Number,
+                        int talon2Number,
+                        int talon3Number,
+                        int talon4Number)
 {
-private:
-	unsigned rotateEncoderLines;
-	unsigned driveEncoderLines;
-	unsigned feetToEncoderLinesRatio;
-	unsigned driveDistance;
-	bool finished;
-	CANTalon *rotateTalon1;
-	CANTalon *rotateTalon2;
-	CANTalon *talon1;
-	CANTalon *talon2;
-	CANTalon *talon3;
-	CANTalon *talon4;
-	Gyro *gyro;
+        rotateEncoderLines = rotateEncLines;
+        driveEncoderLines = driveEncLines;
+        feetToEncoderLinesRatio = feetToEncLinesR;
+        rotateTalon1 = new CANTalon(rotateTalon1Number);
+        rotateTalon2 = new CANTalon(rotateTalon2Number);
+        talon1 = new CANTalon(talon1Number);
+        talon2 = new CANTalon(talon2Number);
+        talon3 = new CANTalon(talon3Number);
+        talon4 = new CANTalon(talon4Number);
+        gyro = new Gyro(gyroChannel);
+        driveDistance = 0;
+        finished = true;
+}
 
-public:
-	SwerveDrive(unsigned rotateEncLines, unsigned driveEncLines,
-				unsigned feetToEncLinesR,
-				unsigned short rotateTalon1Number,
-				unsigned short rotateTalon2Number,
-				unsigned short gyroChannel,
-				int talon1Number,
-				int talon2Number,
-				int talon3Number,
-				int talon4Number)
-	{
-		rotateEncoderLines = rotateEncLines;
-		driveEncoderLines = driveEncLines;
-		feetToEncoderLinesRatio = feetToEncLinesR;
-		rotateTalon1 = new CANTalon(rotateTalon1Number);
-		rotateTalon2 = new CANTalon(rotateTalon2Number);
-		talon1 = new CANTalon(talon1Number);
-		talon2 = new CANTalon(talon2Number);
-		talon3 = new CANTalon(talon3Number);
-		talon4 = new CANTalon(talon4Number);
-		gyro = new Gyro(gyroChannel);
-		driveDistance = 0;
-		finished = true;
-	}
+/*
+ * talon1--talon3
+ * |			|
+ * |			|
+ * talon2--talon4
+ */
+SwerveDrive::~SwerveDrive()
+{
+        delete rotateTalon1;
+        delete rotateTalon2;
+        delete gyro;
+        delete talon1;
+        delete talon2;
+        delete talon3;
+        delete talon4;
+}
 
-	/*
-	 * talon1--talon3
-	 * |			|
-	 * |			|
-	 * talon2--talon4
-	 */
-	~SwerveDrive()
-	{
-		delete rotateTalon1;
-		delete rotateTalon2;
-		delete gyro;
-		delete talon1;
-		delete talon2;
-		delete talon3;
-		delete talon4;
-	}
+/*
+ * angle: the direction relative to the field in which the
+ * robot should drive; field forward is 0/360 degrees
+ *
+ * speed: double ranging from -1.0 to 1.0 which represents the speed at
+ * which the wheels should be driven; -1.0 is full reverse, 1.0 is full forward
+ */
+void SwerveDrive::Drive(int angle, double speed)
+{
+        if (angle == -1 || speed == 0) {
+                talon1->Set(0);
+                talon2->Set(0);
+                talon3->Set(0);
+                talon4->Set(0);
+                rotateTalon1->Set(0);
+                rotateTalon2->Set(0);
+        } // If they joystick is in neutral position or the speed is zero, do nothing.
 
-	/*
-	 * angle: the direction relative to the field in which the
-	 * robot should drive; field forward is 0/360 degrees
-	 *
-	 * speed: double ranging from -1.0 to 1.0 which represents the speed at
-	 * which the wheels should be driven; -1.0 is full reverse, 1.0 is full forward
-	 */
-	void Drive(int angle, double speed)
-	{
-		if (angle == -1 || speed == 0) {
-			talon1->Set(0);
-			talon2->Set(0);
-			talon3->Set(0);
-			talon4->Set(0);
-			rotateTalon1->Set(0);
-			rotateTalon2->Set(0);
-		} // If they joystick is in neutral position or the speed is zero, do nothing.
+        if (TurnRobot(angle, speed))
+        {
+                rotateTalon1->Set(0);
+                rotateTalon2->Set(0);
+                talon1->Set(speed);
+                talon2->Set(speed);
+                talon3->Set(speed);
+                talon4->Set(speed);
+        }	//stops the motors when the wheels are pointing the right way
+                //and tells the drive motors to spin
 
-		if (TurnRobot(angle, speed))
-		{
-			rotateTalon1->Set(0);
-			rotateTalon2->Set(0);
-			talon1->Set(speed);
-			talon2->Set(speed);
-			talon3->Set(speed);
-			talon4->Set(speed);
-		}	//stops the motors when the wheels are pointing the right way
-			//and tells the drive motors to spin
+}
 
-	}
+void SwerveDrive::Stop()
+{
+        Drive(-1, 0);
+}
 
-	void Stop() {
-		Drive(-1, 0);
-	}
+bool SwerveDrive::TurnRobot(int angle, double speed)
+{
 
-	bool TurnRobot(int angle, double speed)
-	{
+        // always rotate the wheels at maximum speed
+        double rotateWheelSpeed = 1.0;
+        //we assume that when (rotateWheelSpeed == 1.0) the wheels will
+        //rotate in a counterclockwise direction
 
-		// always rotate the wheels at maximum speed
-		double rotateWheelSpeed = 1.0;
-		//we assume that when (rotateWheelSpeed == 1.0) the wheels will
-		//rotate in a counterclockwise direction
+        int angleToBeTurned = 0;
 
-		int angleToBeTurned = 0;
+        if (angle > GetWheelAngle())
+                angleToBeTurned = (angle - GetWheelAngle()) % 360;
+        else
+                angleToBeTurned = (GetWheelAngle() - angle) % 360;
 
-		if (angle > GetWheelAngle())
-			angleToBeTurned = (angle - GetWheelAngle()) % 360;
-		else
-			angleToBeTurned = (GetWheelAngle() - angle) % 360;
-
-		if (abs(angleToBeTurned - angle) % 360 > 90)
-		{
-			if ((abs(angleToBeTurned - angle) % 360) > 270)
-				//makes wheels rotate towards the original angle if they're closer to that
-					rotateWheelSpeed *= -1;
-			else	//if the wheels are closer to (180 + angle) than (angle), they turn to (180 + angle)
-			{
-				angleToBeTurned = -(180 - angleToBeTurned) % 360;
-				//finds the opposite of the angle needed to be turned relative to the robot
-				if (angleToBeTurned < 0)
-				{
-					angleToBeTurned *= -1;
-					speed *= -1;
-				}//makes sure wheels rotate the shortest distance
-			}
-		} //the if statement means the wheels won't have to turn over 90 degrees
+        if (abs(angleToBeTurned - angle) % 360 > 90)
+        {
+                if ((abs(angleToBeTurned - angle) % 360) > 270)
+                        //makes wheels rotate towards the original angle if they're closer to that
+                                rotateWheelSpeed *= -1;
+                else	//if the wheels are closer to (180 + angle) than (angle), they turn to (180 + angle)
+                {
+                        angleToBeTurned = -(180 - angleToBeTurned) % 360;
+                        //finds the opposite of the angle needed to be turned relative to the robot
+                        if (angleToBeTurned < 0)
+                        {
+                                angleToBeTurned *= -1;
+                                speed *= -1;
+                        }//makes sure wheels rotate the shortest distance
+                }
+        } //the if statement means the wheels won't have to turn over 90 degrees
 
 
-		int distance = angleToBeTurned * rotateEncoderLines / 360;
+        int distance = angleToBeTurned * rotateEncoderLines / 360;
 
-		if (rotateTalon1->GetEncPosition() != distance % rotateEncoderLines)
-		{
-			rotateTalon1->Set(rotateWheelSpeed);
-			rotateTalon2->Set(rotateWheelSpeed);
-			talon1->Set(0);
-			talon2->Set(0);
-			talon3->Set(0);
-			talon4->Set(0);
-			return false;
-		}	//spins the motors until the wheels point in the right direction and stops drive motors
+        if (rotateTalon1->GetEncPosition() != distance % rotateEncoderLines)
+        {
+                rotateTalon1->Set(rotateWheelSpeed);
+                rotateTalon2->Set(rotateWheelSpeed);
+                talon1->Set(0);
+                talon2->Set(0);
+                talon3->Set(0);
+                talon4->Set(0);
+                return false;
+        }	//spins the motors until the wheels point in the right direction and stops drive motors
 
-		else
-			return true;
-	}
+        else
+                return true;
+}
 
-	bool DriveACertainDistance(double feet, double speed)
-	{
-		if (driveDistance == 0 && finished)
-				driveDistance = feet / feetToEncoderLinesRatio;
+bool SwerveDrive::DriveACertainDistance(double feet, double speed)
+{
+        if (driveDistance == 0 && finished)
+                        driveDistance = feet / feetToEncoderLinesRatio;
 
-		finished = false;
+        finished = false;
 
-		int startPosition = talon1->GetEncPosition();
+        int startPosition = talon1->GetEncPosition();
 
-		talon1->Set(speed);
-		talon2->Set(speed);
-		talon3->Set(speed);
-		talon4->Set(speed);
+        talon1->Set(speed);
+        talon2->Set(speed);
+        talon3->Set(speed);
+        talon4->Set(speed);
 
-		int endPosition = talon1->GetEncPosition();
+        int endPosition = talon1->GetEncPosition();
 
-		driveDistance = driveDistance - abs(endPosition - startPosition);
-		if (driveDistance <= 0)
-		{
-			finished = true;
-			return true;
-		}
+        driveDistance = driveDistance - abs(endPosition - startPosition);
+        if (driveDistance <= 0)
+        {
+                finished = true;
+                return true;
+        }
 
-		return false;
-	}
+        return false;
+}
 
-	void RotateRobot(bool clockwise, double speed) { // Rotate the robot in a given direction by speed 'speed'
-		if (clockwise) {
-			talon1->Set(speed);
-			talon2->Set(speed);
-			talon3->Set(-speed);
-			talon4->Set(-speed);
-		}
-		else {
-			talon1->Set(-speed);
-			talon2->Set(-speed);
-			talon3->Set(speed);
-			talon4->Set(speed);
-		}
-	}
+void SwerveDrive::RotateRobot(bool clockwise, double speed)
+{ // Rotate the robot in a given direction by speed 'speed'
+        if (clockwise) {
+                talon1->Set(speed);
+                talon2->Set(speed);
+                talon3->Set(-speed);
+                talon4->Set(-speed);
+        }
+        else {
+                talon1->Set(-speed);
+                talon2->Set(-speed);
+                talon3->Set(speed);
+                talon4->Set(speed);
+        }
+}
 
-	int GetWheelAngle()
-	{
-		float gyroangle = gyro->GetAngle();
-		int robotRelativeWheelAngle = rotateTalon1->GetEncPosition() * 360 / rotateEncoderLines;
-		int fieldRelativeWheelAngle = (gyroangle + robotRelativeWheelAngle) % 360;
-		return fieldRelativeWheelAngle;
-	}	//finds the angle the wheels are facing relative to the field
-};
+int SwerveDrive::GetWheelAngle()
+{
+        float gyroangle = gyro->GetAngle();
+        int robotRelativeWheelAngle = rotateTalon1->GetEncPosition() * 360 / rotateEncoderLines;
+        int fieldRelativeWheelAngle = (gyroangle + robotRelativeWheelAngle) % 360;
+        return fieldRelativeWheelAngle;
+}	//finds the angle the wheels are facing relative to the field
