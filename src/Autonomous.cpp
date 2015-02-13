842bcb2caa4803def9f08d12c9e15a6db361e948
Kaz Smith <icroboticsteam167@googlegroups.com>
2015-02-13 17:32:45 -0600

    Begin adding autonomous code to the mechanum drive code

--- src/Autonomous.cpp
+++ src/Autonomous.cpp
@@ -7,4 +7,43 @@
 
 #include "Robot.h"
 
-//will be filled in with code from the AutonomousBranch when merged
+bool Robot::PickUpTote()
+{
+        elevatorArms->Close();
+        elevatorArms->Raise(1.0)
+        mechanumWheels->Right();
+        if (done)
+                elevatorArms->Open();
+        return done;
+}
+
+void Robot::Stop()
+{
+        swerveWheels->Stop();
+        elevatorArms->Stop();
+}
+
+bool Robot::DriveForward(double distance)
+{
+        return swerveWheels->DriveACertainDistance(distance, 1.0); //drive 8.92 feet and at speed 1.0 (full speed)
+}
+
+void Robot::StackBin ()
+{
+	elevatorArms->Raise(1.0);
+	elevatorArms->Open();
+	elevatorArms->Close();
+	elevatorArms->Stop();
+}
+
+bool Robot::Turn(int angle)
+{
+	return swerveWheels->TurnRobot(angle);
+}
+
+bool Robot::Rotate(int gyroAngle, bool clockwise)
+{
+	swerveWheels->RotateRobot(clockwise, 1.0);
+	return swerveWheels->GetGyroAngle();
+
+}
