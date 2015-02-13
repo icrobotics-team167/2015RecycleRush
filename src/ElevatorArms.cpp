7c1ba8eeae241ca5eff954eaa70b9956e150b263
Kaz Smith <mr.hexor@gmail.com>
2015-02-13 17:24:23 -0600

    code formatting

--- src/ElevatorArms.cpp
+++ src/ElevatorArms.cpp
@@ -1,54 +1,54 @@
 /*
- * ElevatorArms.cpp
- *
- *  Created on: Jan 15, 2015
- *      Author: Katie Chace
- */
+* ElevatorArms.cpp
+*
+*  Created on: Jan 15, 2015
+*  Author: Katie Chace
+*/
 
 #include "ElevatorArms.h"
 
 ElevatorArms::ElevatorArms(int compressorIndex,
-                int solenoidIndex1_1, int solenoidIndex1_2,
-                int talonPWMchannel)
+							int solenoidIndex1_1, int solenoidIndex1_2,
+							int talonPWMchannel)
 
 {
-        piston1 = new PneumaticPiston(compressorIndex, solenoidIndex1_1, solenoidIndex1_2);
-        talon = new Talon(talonPWMchannel);
+	piston1 = new PneumaticPiston(compressorIndex, solenoidIndex1_1, solenoidIndex1_2);
+	talon = new Talon(talonPWMchannel);
 
 }
 
 ElevatorArms::~ElevatorArms()
 {
-        delete piston1;;
-        delete talon;
+	delete piston1;;
+	delete talon;
 }
 
 void ElevatorArms::Raise(float speed)
 {
-        talon->Set(abs(speed));
+	talon->Set(abs(speed));
 }
 
 void ElevatorArms::Lower(float speed)
 {
-        talon->Set(-(abs(speed)));
+	talon->Set(-(abs(speed)));
 }
 
 void ElevatorArms::StopElevator()
 {
-        talon->Set(0);
+	talon->Set(0);
 }
 
 void ElevatorArms::Open()
 {
-        piston1->Extend();
+	piston1->Extend();
 }
 
 void ElevatorArms::Close()
 {
-        piston1->Retract();
+	piston1->Retract();
 }
 
 void ElevatorArms::Stop()
 {
-        piston1->ForceStop();
+	piston1->ForceStop();
 }
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -5,28 +5,28 @@ using std::endl;
 
 Robot::Robot()
 {
-        RealJoy1 = new Joystick(1);
-        RealJoy2 = new Joystick(2);
-        Joystick1 = new SimpleJoystick(RealJoy1);
-        Joystick2 = new SimpleJoystick(RealJoy2);
+	RealJoy1 = new Joystick(1);
+	RealJoy2 = new Joystick(2);
+	Joystick1 = new SimpleJoystick(RealJoy1);
+	Joystick2 = new SimpleJoystick(RealJoy2);
 
-        // current parameters are just placeholders for actual values
-        elevatorArms = new ElevatorArms(3, 1, 4, 4);
+	// current parameters are just placeholders for actual values
+	elevatorArms = new ElevatorArms(3, 1, 4, 4);
 
-        // current parameters are actual values for the mechanum robot
-        mechanumWheels = new MechanumDrive(7, 1, 9, 2, 1.0);
+	// current parameters are actual values for the mechanum robot
+	mechanumWheels = new MechanumDrive(7, 1, 9, 2, 1.0);
 
-        autoState = PICK_UP_TOTE;
+	autoState = PICK_UP_TOTE;
 }
 
 Robot::~Robot()
 {
-        delete RealJoy1;
-        delete RealJoy2;
-        delete Joystick1;
-        delete Joystick2;
-        delete mechanumWheels;
-        delete elevatorArms;
+	delete RealJoy1;
+	delete RealJoy2;
+	delete Joystick1;
+	delete Joystick2;
+	delete mechanumWheels;
+	delete elevatorArms;
 }
 
 void Robot::RobotInit()
@@ -51,8 +51,8 @@ void Robot::TeleopInit()
 
 void Robot::TeleopPeriodic()
 {
-        JoystickOne();
-        JoystickTwo();
+	JoystickOne();
+	JoystickTwo();
 }
 
 void Robot::JoystickOne() {
@@ -66,11 +66,14 @@ void Robot::JoystickOne() {
 	float x = this->RealJoy1->GetAxis(Joystick::kXAxis);
 	float y = -this->RealJoy1->GetAxis(Joystick::kYAxis);
 	float z = Vector3::GetRotation(x, y);
+
 	// raw axis 3 is the twist axis on the Logitech Extreme 3D Pro joystick
 	// we use the raw axis because the default mappings are incorrect
 	//float twist = this->RealJoy1->GetRawAxis(3);
+
 	// Set the throttle
 	bool turbo = Joystick1->Toggled(BUTTON_8);
+
 	/*
 	* raw axis 4 is the throttle axis on the Logitech Extreme 3D Pro joystick
 	* we use the raw axis because the default mappings are incorrect
@@ -81,108 +84,119 @@ void Robot::JoystickOne() {
 	*/
 	double throttle_mag = (this->RealJoy1->GetThrottle() * -1.0 + 1.0) / 2.0;
 	SmartDashboard::PutNumber("throttle", throttle_mag);
+
 	float abs_x = abs(x), abs_y = abs(y);
+
 	double voltagePercent = throttle_mag;
+
 	if (!Joystick1->Pressed(BUTTON_5) && !Joystick1->Pressed(BUTTON_6))
 	{
 	// if we are not turning get the larger of the x and y values of the joystick posistion,
 	// and multiply that by the throttle to get final voltage
-	if (abs_x > abs_y)
-		voltagePercent *= throttle_mag * abs_x;
-	else
-		voltagePercent *= throttle_mag * abs_y;
+		if (abs_x > abs_y)
+			voltagePercent *= throttle_mag * abs_x;
+		else
+			voltagePercent *= throttle_mag * abs_y;
 	}
 	else
 	{
-	// if we are turning, the rate of turning depends only on the throttle setting,
-	// and the rate of turning is limited to 80% voltage maximum
-	voltagePercent *= 0.8;
+		// if we are turning, the rate of turning depends only on the throttle setting,
+		// and the rate of turning is limited to 80% voltage maximum
+		voltagePercent *= 0.8;
 	}
+
 	if (voltagePercent < 0.1)
-	voltagePercent = 0.1;
+		voltagePercent = 0.1;
+
 	if (!turbo)
-	voltagePercent *= 0.75;
+		voltagePercent *= 0.75;
+
 	if (voltagePercent > 1.0)
-	voltagePercent = 1.0;
+		voltagePercent = 1.0;
+
 	mechanumWheels->SetVoltagePercent(voltagePercent);
+
+	/*
 	cout << "volt" << voltagePercent << endl;
 	cout << "throttle" << throttle_mag << endl;
+	*/
+
 	if (Joystick1->Pressed(BUTTON_6))
 	{
-	// rotate right
-	mechanumWheels->RotateLeft();
+		// rotate right
+		mechanumWheels->RotateLeft();
 	}
 	else if (Joystick1->Pressed(BUTTON_5))
 	{
-	// rotate left
-	mechanumWheels->RotateRight();
+		// rotate left
+		mechanumWheels->RotateRight();
 	}
 	else if (Vector3::GetMagnitude(x, y) < 0.25)
 	{
-	// stop
-	mechanumWheels->Stop();
+		// stop
+		mechanumWheels->Stop();
 	}
 	else if (z >= 225 && z < 315)
 	{
-	// forward
-	mechanumWheels->Reverse();
+		// forward
+		mechanumWheels->Reverse();
 	}
 	else if ((z >= 315 && z <= 360) || (z >= 0 && z < 45))
 	{
-	// right
-	mechanumWheels->Left();
+		// right
+		mechanumWheels->Left();
 	}
 	else if(z >= 45 && z < 135)
 	{
-	// backwards
-	mechanumWheels->Forward();
+		// backwards
+		mechanumWheels->Forward();
 	}
 	else if (z >= 135 && z < 225)
 	{
-	// left
-	mechanumWheels->Right();
+		// left
+		mechanumWheels->Right();
 	}
 	else
 	{
-	// stop
-	mechanumWheels->Stop();
+		// stop
+		mechanumWheels->Stop();
 	}
-
 }
 
 void Robot::JoystickTwo() {
-        // Joy2 Control Code
+	// Joy2 Control Code
 
-        float y2 = -this->RealJoy2->GetAxis(Joystick::kYAxis);
-        double throttle_mag2 = (this->RealJoy2->GetRawAxis(4) * -1.0 + 1.0) / 2.0;
-        float abs_y2 = abs(y2);
-        float armSpeed = throttle_mag2 * abs_y2;
+	float y2 = -this->RealJoy2->GetAxis(Joystick::kYAxis);
+	double throttle_mag2 = (this->RealJoy2->GetRawAxis(4) * -1.0 + 1.0) / 2.0;
+	float abs_y2 = abs(y2);
+	float armSpeed = throttle_mag2 * abs_y2;
 
-        if (armSpeed < 0)
-                elevatorArms->Raise(armSpeed);
-        else
-                elevatorArms->Lower(armSpeed);
+	if (armSpeed < 0)
+        elevatorArms->Raise(armSpeed);
+	else
+        elevatorArms->Lower(armSpeed);
 
-        // Deprecated code
-        /*bool open = Joystick2->Toggled(BUTTON_3);
-        bool close = Joystick2->Toggled(BUTTON_1);
+	// Deprecated code
+	/*bool open = Joystick2->Toggled(BUTTON_3);
+	bool close = Joystick2->Toggled(BUTTON_1);
 
-        if (open)
-                elevatorArms->Open();
-        if (close)
-                elevatorArms->Close();
+	if (open)
+        elevatorArms->Open();
+	if (close)
+        elevatorArms->Close();
 
-        if ((!open && !close) || (open && close))
-                elevatorArms->Stop();*/
+	if ((!open && !close) || (open && close))
+        elevatorArms->Stop();*/
 
-        if (Joystick2->Toggled(BUTTON_1))					// Trigger - Open all
-                elevatorArms->Close();
-        else {
-                if (Joystick2->Toggled(BUTTON_8))
-                        elevatorArms->Close();						// Button 7/8 - Open/close piston 1
-                else if (Joystick2->Toggled(BUTTON_7))
-                        elevatorArms->Open();
-        }
+	if (Joystick2->Toggled(BUTTON_1))                                        // Trigger - Open all
+        elevatorArms->Close();
+	else
+	{
+        if (Joystick2->Toggled(BUTTON_8))
+			elevatorArms->Close();                                                // Button 7/8 - Open/close piston 1
+        else if (Joystick2->Toggled(BUTTON_7))
+			elevatorArms->Open();
+	}
 }
 
 START_ROBOT_CLASS(Robot);
